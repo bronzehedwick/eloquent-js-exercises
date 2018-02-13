da208c0bcd6b61e17cf31bc2254cd0a91db0885a
Chris DeLuca <delucac@mskcc.org>
2018-02-13 16:05:26 -0500

    Add fizzbuzz test and code

---
+++ src/chapter-02/fizzbuzz.js
@@ -0,0 +1,20 @@
+function fizzbuzz() {
+  var accumulator;
+  var output = '';
+  for (let i = 1; i < 101; i++) {
+    accumulator = i;
+    if (i % 3 == 0) {
+      accumulator = 'Fizz';
+    }
+    if (i % 5 == 0) {
+      if (typeof accumulator === 'number') {
+        accumulator = '';
+      }
+      accumulator += 'Buzz';
+    }
+    output += accumulator + '\n';
+  }
+  return output;
+}
+
+module.exports = fizzbuzz;
