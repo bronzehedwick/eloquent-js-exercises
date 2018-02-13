21e09a335d42e31cd427f1da8f7558cf70347b24
Chris DeLuca <delucac@mskcc.org>
2018-02-13 16:23:16 -0500

    Add chess board code and test

---
+++ __tests__/chapter-02/chess-board.test.js
@@ -0,0 +1,11 @@
+const chessBoard = require('../../src/chapter-02/chess-board.js');
+const expectedOutput = `# # # # # # #
+ # # # # # # 
+# # # # # # #
+ # # # # # # 
+# # # # # # #
+`;
+
+test('chess board the right number of squares', () => {
+  expect(chessBoard(13, 5)).toBe(expectedOutput);
+});
