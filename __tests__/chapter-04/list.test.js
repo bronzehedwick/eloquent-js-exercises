936ed9a1be025673dd027de0a9b4d7011d7e5521
Chris DeLuca <delucac@mskcc.org>
2018-02-16 16:12:47 -0500

    Add list to array tests and code

---
+++ __tests__/chapter-04/list.test.js
@@ -0,0 +1,26 @@
+const lists = require('../../src/chapter-04/list.js');
+const arrayToList = lists.arrayToList;
+const listToArray = lists.listToArray;
+const expectedArray = [1, 2, 3];
+const expectedList = {
+  value: 1,
+  rest: {
+    value: 2,
+    rest: {
+      value: 3,
+      rest: null
+    }
+  }
+};
+
+describe('lists', () => {
+
+  test('converts arrays to lists', () => {
+    expect(arrayToList([1,2,3])).toEqual(expectedList);
+  });
+
+  test('converts lists to arrays', () => {
+    expect(listToArray(expectedList)).toEqual([1,2,3]);
+  });
+
+});
