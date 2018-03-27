b5035fc07eb9ac74df9691538f5e8fbbd81d0726
Chris DeLuca <delucac@mskcc.org>
2018-03-27 13:44:16 -0400

    Make flatten code more elegant
    After looking at the eloquent js author's solution, which actually only
    covers nested array values, instead of other values as well, I
    discovered a handy `reduce` option which allows the setting of the
    initial accumulator value.

--- src/chapter-05/flatten.js
+++ src/chapter-05/flatten.js
@@ -1,11 +1,9 @@
 function flatten(arr) {
-  // Make sure the array's first element is another array to reduce into.
-  arr.unshift([]);
   // Reduce all arrays into a single array.
   return arr.reduce((accumulator, currentValue) => {
     if (Array.isArray(currentValue)) return accumulator.concat(flatten(currentValue));
     return accumulator.concat(currentValue);
-  });
+  }, []);
 }
 
 module.exports = flatten;
