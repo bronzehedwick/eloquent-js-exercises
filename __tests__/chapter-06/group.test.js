35de0d96bddb944c7dd1a712fd6c1f1989ffc6aa
Chris DeLuca <code@chrisdeluca.me>
2021-03-19 16:21:18 -0400

    Add group test and scaffolding

---
+++ __tests__/chapter-06/group.test.js
@@ -0,0 +1,13 @@
+const Group = require('../../src/chapter-06/group.js');
+let group = Group.from([10, 20]);
+
+test('has correct values', () => {
+    expect(group.has(10)).toBe(true);
+    expect(group.has(30)).toBe(false);
+});
+
+test('adds/deletes values', () => {
+    group.add(10);
+    group.delete(10);
+    expect(group.has(10)).toBe(false);
+});
